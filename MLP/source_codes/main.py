64c0880e4b73db0a80a874aee708306058031c1a
Subhankar Chakraborty <SubhankarChakraborty48@gmail.com>
2019-08-21 23:11:14 +0530

    declaring the hidden layer class

--- MLP/source_codes/main.py
+++ MLP/source_codes/main.py
@@ -1,3 +1,4 @@
+import neuralnet as nn
 
 from activations import map_of_functions
 from derivatives import map_of_derivatives
--- MLP/source_codes/neuralnet.py
+++ MLP/source_codes/neuralnet.py
@@ -3,3 +3,15 @@ import numpy as np
 from activations import map_of_functions
 from derivatives import map_of_derivatives
 
+class hidden_layer(object):
+    def __inti__(self, no_of_neurons, activation_function = 'sigmoid'):
+        self.size = no_of_neurons
+
+        if activation_function in ["sigmoid", "ReLU", "softmax", "tanh"]:
+            self.activation = map_of_functions[activation_function]
+            self.derivative = mao_of_derivatives[activation_function]
+        
+        else:
+            print("The activation function mentioned does not exist.")
+            exit(0)
+    
