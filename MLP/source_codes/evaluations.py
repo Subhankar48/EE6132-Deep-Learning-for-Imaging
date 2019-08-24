c2efedf43914113df8b36c2a11c956dee7718eed
Subhankar Chakraborty <SubhankarChakraborty48@gmail.com>
2019-08-24 14:40:29 +0530

    add the evaluations.py file

--- MLP/source_codes/evaluations.py
+++ MLP/source_codes/evaluations.py
@@ -1,5 +1,13 @@
 import numpy as np
 
 def precision(y_pred, y_true):
+    np.sum(np.sum(y_pred*y_true))/np.sum(np.sum(y_pred))
+
+def recall(y_pred, y_true):
+    np.sum(np.sum(y_pred*y_true))/np.sum(np.sum(y_true))
+
+def f1_score(y_pred, y_true):
+    _precision = precision(y_pred,y_true)
+    _recall = recall(y_pred, y_true)
+    return 2*_precision*_recall/(_precision+_recall)
     
-    pass
