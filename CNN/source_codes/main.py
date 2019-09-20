6b7a2319bca327a71cada6fad7a4d7243ed110ad
Subhankar Chakraborty <SubhankarChakraborty48@gmail.com>
2019-09-20 13:07:58 +0530

    git push

--- CNN/source_codes/main.py
+++ CNN/source_codes/main.py
@@ -1,26 +1,45 @@
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from torchvision import datasets, transforms
-import os
+from torch.utils.data import DataLoader, Dataset, TensorDataset
 
+
+# Configurations
 CURRENT_DIRECTORY = os.getcwd()
 FOLDER_NAME = 'mnist'
+to_download = False
+_batch_size = 64
+_shuffle = True
+VALIDATION_SIZE = 10000
+use_cuda = torch.cuda.is_available()
+computation_device = torch.device("cuda" if use_cuda else "cpu")
 
+# Download
 if not os.path.exists(os.path.join(CURRENT_DIRECTORY, FOLDER_NAME)):
     os.mkdir(os.path.join(CURRENT_DIRECTORY, FOLDER_NAME))
+    to_download = True
+
+# Main
 
 
 def main():
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-    train_loader = torch.utils.data.DataLoader(datasets.MNIST(FOLDER_NAME, train=True, download=True,
-                                                              transform=transforms.Compose([
-                                                                  transforms.ToTensor(),
-                                                                  transforms.Normalize(
-                                                                      (0.1307,), (0.3081,))
-                                                              ])))
+    train_loader = DataLoader(datasets.MNIST(FOLDER_NAME, train=True, download=to_download,
+                                             transform=transforms.Compose([
+                                                 transforms.ToTensor(),
+                                                 transforms.Normalize(
+                                                     (0.1307,), (0.3081,))
+                                             ])), shuffle=_shuffle)
+    test_loader = DataLoader(
+        datasets.MNIST(FOLDER_NAME, train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=_batch_size, shuffle=_shuffle)
 
 
 if __name__ == '__main__':
